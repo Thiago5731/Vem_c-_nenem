4fa5a39ea6f0851bae623bfe04e1ce62803dc530
Thiago5731 <thiagov88@outlook.com>
2025-10-16 06:29:55 -0300

    Add files via upload

---
+++ musica.py
@@ -0,0 +1,16 @@
+import pygame
+import time 
+
+def segundos(tempo):
+    time.sleep(tempo)
+
+
+print ('Vem cá neném, ')
+print ('pros braços do papai')
+print ('Papai te dá colinho, ')
+print ('da beijinho e muito mais')
+print ('Vem vem vem, ')
+print ('vem vem neném')
+print ('Seu pai te ama')
+print ('Vou da tapa no bumbum')
+print ('Neném dorme eu levo pra cama')
